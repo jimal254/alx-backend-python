535792a3069a38f61a646bc478a0d22224c483db
RamzyAR7 <rameya683@gmail.com>
2024-08-02 19:12:58 +0300

    add v7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 7-to_kv.py """
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """_summary_
+
+    Args:
+        k (str): STR
+        v (Union[int, float]): Int OR float 
+
+    Returns:
+        Tuple: _description_
+    """
+    v: float = v**2
+    return (k, v)
