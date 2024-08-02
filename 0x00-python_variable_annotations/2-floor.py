1f77cd5f07151a77a1fdb3f671561be16f940e9e
RamzyAR7 <rameya683@gmail.com>
2024-08-02 18:32:35 +0300

    add v2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 2-floor module """
+
+
+def floor(n :float) -> int:
+    """
+    takes a float n as argument and
+    returns the floor of the float.
+    """
+    return round(n)
