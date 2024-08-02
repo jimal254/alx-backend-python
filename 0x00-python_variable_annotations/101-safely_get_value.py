7922f03d51d2e53243afcff40a61f61f21760c82
RamzyAR7 <rameya683@gmail.com>
2024-08-02 20:03:02 +0300

    add v101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-""" 101-safely_get_value.py """
-
+"""Advanced type annotated function"""
 from typing import Any, Mapping, TypeVar, Union
-
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """_summary_
 
-    Args:
-        dct (Mapping): _description_
-        key (Any): _description_
-        default (Union[T, None], optional): _description_. Defaults to None.
-
-    Returns:
-        Union[Any, T]: _description_
-    """
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
+                     = None) -> Union[Any, T]:
+    """More type annotations"""
     if key in dct:
         return dct[key]
     else:
